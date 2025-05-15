8d848bcdfa08905a80c645b32881f6248a161bbc
egecan12 <egecan18k@gmail.com>
2025-05-15 21:40:18 +0200

    Add Google site verification meta tag in layout component

---
+++ src/app/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="google-site-verification" content="Jp8ddzFpFUI9omm4o1Icb_Z3TJSuBZKIyBFq5X8" />
+    </>
+  )
+} 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,9 +42,6 @@ export const metadata: Metadata = {
       follow: true,
       'max-image-preview': 'large'
     }
-  },
-  verification: {
-    google: 'Jp8ddzFpFUI9omm4o1Icb_Z3TJSuBZKIyBFq5X8',
   }
 };
 
@@ -55,6 +52,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="Jp8ddzFpFUI9omm4o1Icb_Z3TJSuBZKIyBFq5X8" />
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
